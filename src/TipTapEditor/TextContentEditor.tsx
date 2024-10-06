db6cf443762c9ff0c8eb2be3ee3a7116e79a5679
Awais Muzaffar <mail@awaismuzaffar.com>
2024-10-06 16:53:11 +0100

    comment out for now

--- src/TipTapEditor/TextContentEditor.tsx
+++ src/TipTapEditor/TextContentEditor.tsx
@@ -31,10 +31,10 @@ const TextContentEditor: React.FC<TipTapEditorProps> = ({ content, onUpdate, onF
         onFocus(editor);
       },
       onBlur({event}) {
-        if (!event.relatedTarget || !(event.relatedTarget instanceof HTMLElement && event.relatedTarget.closest('.TextContentEditor'))) {
-          // Refocus the editor if blur wasn't caused by focus on another editor
-          editor?.chain().focus().run();
-        }
+        // if (!event.relatedTarget || !(event.relatedTarget instanceof HTMLElement && event.relatedTarget.closest('.TextContentEditor'))) {
+        //   // Refocus the editor if blur wasn't caused by focus on another editor
+        //   editor?.chain().focus().run();
+        // }
 
       }
     });
